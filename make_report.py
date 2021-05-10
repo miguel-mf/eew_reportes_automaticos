ea53afbbf2bcb3cd807254e02d8701f44526b77f
miguel-mf <59932523+miguel-mf@users.noreply.github.com>
2021-05-10 18:43:53 -0400

    Update make_report.py

--- make_report.py
+++ make_report.py
@@ -2,6 +2,9 @@ from reportSettings import *
 file_name = "reporte.pdf"
 title = "Reporte alerta temprana"
 
+Fecha_Inicio = 'DD-MM-YYYY'  # Formato: DD-MM-YYYY
+Fecha_Termino = 'DD-MM-YYYY' # Formato: DD-MM-YYYY
+
 # TAMAÑO A4 = 210 x 297
 # Palabras a reemplazar:
 # PERIODO_PERIODO: mes, año, periodo
@@ -23,11 +26,16 @@ pdf.image('images/Figura_Sismicidad.png', 20, 160, 170, 85)
 pdf.caption('Figura 1: Catalogo CSN sismos M>4.0 desde 2013 hasta 2020 en el norte de Chile.',245)
 pdf.texts('text/contexto2.txt', 270)
 
-pdf.append_chapter(2, 'Funcionamiento del sistema de alerta temprana', 'text/reporte1.txt')
+pdf.append_chapter(2, 'Sismicidad registrada y alertas emitidas', 'text/reporte1.txt')
 #pdf.add_page()
 pdf.image('images/Template.png', 15, 45, 85, 85) # SISMICIDAD REGISTRADA
 pdf.image('images/Template.png', 110, 45, 85, 85) # SISMICIDAD ALERTADA
-pdf.caption('Figure 2: Izquierda: Sismicidad registrada durante el mes de Octubre 2020. Derecha: Alertas emitidas asociadas a sismos.',130)
+pdf.caption('Figure 2: Izquierda: Sismicidad registrada durante el PERIODO_PERIODO de PERIODO_TIEMPO. Derecha: Alertas emitidas asociadas a sismos.',130)
+pdf.texts('text/reporte2.txt', 135) # Parrafo sobre las alertas emitidas y tiempos de alerta
+
+pdf.print_chapter(3, '', 'text/contexto1.txt')
+
+pdf.image('images/Template.png', 110, 45, 85, 85) # Errores de: magnitud, tiempo origen, distancia, profundidad
 
 pdf.output(file_name, 'F')
 
