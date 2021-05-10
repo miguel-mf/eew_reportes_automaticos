67b18b6d56a29c6fdc4aafae764f63fea757123f
miguel-mf <59932523+miguel-mf@users.noreply.github.com>
2021-05-10 13:13:54 -0400

    Update make_report.py

--- make_report.py
+++ make_report.py
@@ -77,10 +77,10 @@ class PDF(FPDF):
         self.multi_cell(0,7,txt)
 
 pdf = PDF()
-pdf.titles(title)
-pdf.logo('images/CSN.png', 0, 0, 60, 30)
+#pdf.titles(title)
 pdf.set_author('Miguel Medina Flores')
 pdf.print_chapter(1, 'Contexto sísmico del norte de Chile', 'text/contexto1.txt')
+pdf.logo('images/CSN.png', 0, 0, 60, 30)
 pdf.image('images/Figura_Sismicidad.png', 20, 135, 170, 85)
 pdf.caption('Figura 1: Catalogo CSN sismos M>4.0 desde 2013 hasta 2020 en el norte de Chile.',220)
 pdf.texts('text/contexto2.txt', 240)
